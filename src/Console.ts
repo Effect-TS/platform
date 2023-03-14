f98375d174cfbdd7c2f46067b37c5c6610d9c336
Maxwell Brown <maxwellbrown1990@gmail.com>
2023-03-13 21:16:30 -0400

    start work on file system

--- src/Console.ts
+++ src/Console.ts
@@ -2,13 +2,14 @@
  * @since 1.0.0
  */
 import * as Context from "@effect/data/Context"
+import * as Effect from "@effect/io/Effect"
 
 /**
  * @since 1.0.0
  * @category models
  */
 export interface Console {
-  printLine(message: any): void
+  log(message: any): void
 }
 
 /**
@@ -16,3 +17,10 @@ export interface Console {
  * @category context
  */
 export const Console: Context.Tag<Console> = Context.Tag<Console>()
+
+/**
+ * @since 1.0.0
+ * @category accessors
+ */
+export const log = (message: any): Effect.Effect<Console, never, void> =>
+  Effect.serviceWith(Console, (console) => console.log(message))
