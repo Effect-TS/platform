b9592215bd53e68d004ec22be0f92a58068ce524
Tim <hello@timsmart.co>
2023-06-05 19:47:44 +1200

    chore: remove Console accessors

--- src/Console.ts
+++ src/Console.ts
@@ -2,7 +2,6 @@
  * @since 1.0.0
  */
 import * as Context from "@effect/data/Context"
-import * as Effect from "@effect/io/Effect"
 
 /**
  * @since 1.0.0
@@ -16,11 +15,4 @@ export interface Console {
  * @since 1.0.0
  * @category context
  */
-export const Console: Context.Tag<Console> = Context.Tag<Console>()
-
-/**
- * @since 1.0.0
- * @category accessors
- */
-export const log = (message: any): Effect.Effect<Console, never, void> =>
-  Effect.serviceWith(Console, (console) => console.log(message))
+export const Console: Context.Tag<Console, Console> = Context.Tag<Console>()
