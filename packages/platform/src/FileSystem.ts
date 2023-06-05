abaef82d5f8120d2f5c8f61e6d3375fd02c4c071
Tim <hello@timsmart.co>
2023-06-06 11:03:19 +1200

    chore: FileSystem api

--- packages/platform/src/FileSystem.ts
+++ packages/platform/src/FileSystem.ts
@@ -1,6 +1,7 @@
 /**
  * @since 1.0.0
  */
+import type * as Brand from "@effect/data/Brand"
 import type * as Data from "@effect/data/Data"
 import type { Option } from "@effect/data/Option"
 import type * as Effect from "@effect/io/Effect"
@@ -30,6 +31,16 @@ export interface FileSystemError extends Data.Case {
 
 /**
  * @since 1.0.0
+ * @category models
+ */
+export interface FileSyscallError extends Data.Case {
+  readonly [FileSystemErrorId]: typeof FileSystemErrorId
+  readonly _tag: "FileSyscallError"
+  readonly method: string
+}
+
+/**
+ * @since 1.0.0
  * @category constructor
  */
 export const FileSystemError: Data.Case.Constructor<FileSystemError, "_tag" | FileSystemErrorId> =
@@ -39,8 +50,27 @@ export const FileSystemError: Data.Case.Constructor<FileSystemError, "_tag" | Fi
  * @since 1.0.0
  * @category models
  */
+type FileDescriptor = Brand.Branded<number, "FileDescriptor">
+
+/**
+ * @since 1.0.0
+ * @category models
+ */
 export interface File {
+  readonly fd: FileDescriptor
   readonly stat: Effect.Effect<never, FileSystemError, FileInfo>
+  readonly read: (buffer: Uint8Array, options?: FileReadOptions) => Effect.Effect<never, FileSystemError, number>
+  readonly truncate: (length?: number) => Effect.Effect<never, FileSystemError, void>
+  readonly write: (buffer: Uint8Array) => Effect.Effect<never, FileSystemError, number>
+}
+
+/**
+ * @since 1.0.0
+ * @category models
+ */
+export interface FileReadOptions {
+  readonly offset?: number
+  readonly length?: number
 }
 
 /**
@@ -74,6 +104,54 @@ export interface FileInfo {
  * @since 1.0.0
  * @category models
  */
+export interface AccessFileOptions {
+  readonly ok?: boolean
+  readonly readable?: boolean
+  readonly writable?: boolean
+}
+
+/**
+ * @since 1.0.0
+ * @category models
+ */
+export interface DirectoryEntry {
+  readonly name: string
+  readonly isDirectory: boolean
+  readonly isFile: boolean
+  readonly isSymbolicLink: boolean
+}
+
+/**
+ * @since 1.0.0
+ * @category models
+ */
+export interface MakeDirectoryOptions {
+  readonly recursive?: boolean
+  readonly mode?: number
+}
+
+/**
+ * @since 1.0.0
+ * @category models
+ */
+export interface MakeTempDirOptions {
+  readonly directory?: string
+  readonly prefix?: string
+}
+
+/**
+ * @since 1.0.0
+ * @category models
+ */
+export interface MakeTempFileOptions {
+  readonly directory?: string
+  readonly prefix?: string
+}
+
+/**
+ * @since 1.0.0
+ * @category models
+ */
 export interface OpenFileOptions {
   readonly read?: boolean
   readonly write?: boolean
@@ -88,6 +166,75 @@ export interface OpenFileOptions {
  * @since 1.0.0
  * @category models
  */
+export interface ReadDirectoryOptions {
+  readonly recursive?: boolean
+}
+
+/**
+ * @since 1.0.0
+ * @category models
+ */
+export interface RemoveOptions {
+  readonly recursive?: boolean
+}
+
+/**
+ * @since 1.0.0
+ * @category models
+ */
+export interface WriteFileOptions {
+  readonly append?: boolean
+  readonly create?: boolean
+  readonly createNew?: boolean
+  readonly mode?: number
+}
+
+/**
+ * @since 1.0.0
+ * @category models
+ */
 export interface FileSystem {
-  open(path: string, options?: OpenFileOptions): Effect.Effect<Scope, FileSystemError, File>
+  readonly access: (path: string, options?: AccessFileOptions) => Effect.Effect<never, FileSystemError, void>
+  readonly copyFile: (
+    fromPath: string,
+    toPath: string
+  ) => Effect.Effect<never, FileSystemError, void>
+  readonly chmod: (path: string, mode: number) => Effect.Effect<never, FileSystemError, void>
+  readonly chown: (path: string, uid: number, gid: number) => Effect.Effect<never, FileSystemError, void>
+  readonly link: (
+    fromPath: string,
+    toPath: string
+  ) => Effect.Effect<never, FileSystemError, void>
+  readonly makeTempDir: (options?: MakeTempDirOptions) => Effect.Effect<never, FileSystemError, string>
+  readonly makeTempFile: (options?: MakeTempFileOptions) => Effect.Effect<never, FileSystemError, string>
+  readonly makeDirectory: (path: string, options?: MakeDirectoryOptions) => Effect.Effect<never, FileSystemError, void>
+  readonly open: (path: string, options?: OpenFileOptions) => Effect.Effect<Scope, FileSystemError, File>
+  readonly readDirectory: (
+    path: string,
+    options?: ReadDirectoryOptions
+  ) => Effect.Effect<never, FileSystemError, Uint8Array>
+  readonly readFile: (path: string) => Effect.Effect<never, FileSystemError, Uint8Array>
+  readonly readLink: (path: string) => Effect.Effect<never, FileSystemError, string>
+  readonly realPath: (path: string) => Effect.Effect<never, FileSystemError, string>
+  readonly remove: (path: string, options?: RemoveOptions) => Effect.Effect<never, FileSystemError, void>
+  readonly rename: (oldPath: string, newPath: string) => Effect.Effect<never, FileSystemError, void>
+  readonly stat: (path: string) => Effect.Effect<never, FileSystemError, FileInfo>
+  readonly symlink: (
+    fromPath: string,
+    toPath: string
+  ) => Effect.Effect<never, FileSystemError, void>
+  readonly truncate: (
+    path: string,
+    length?: number
+  ) => Effect.Effect<never, FileSystemError, void>
+  readonly utime: (
+    path: string,
+    atime: Date | number,
+    mtime: Date | number
+  ) => Effect.Effect<never, FileSystemError, void>
+  readonly writeFile: (
+    path: string,
+    data: Uint8Array,
+    options?: WriteFileOptions
+  ) => Effect.Effect<never, FileSystemError, void>
 }
