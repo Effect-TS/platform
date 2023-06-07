03680082b358a5b5bd06492df2cba676ec551901
Tim <hello@timsmart.co>
2023-06-08 10:55:19 +1200

    wip

--- packages/platform/src/FileSystem.ts
+++ packages/platform/src/FileSystem.ts
@@ -16,89 +16,199 @@ import type { Stream } from "@effect/stream/Stream"
  * @category model
  */
 export interface FileSystem {
+  /**
+   * Check if a file can be accessed.
+   * You can optionally specify the level of access to check for.
+   */
   readonly access: (
     path: string,
     options?: AccessFileOptions
   ) => Effect.Effect<never, PlatformError, void>
+  /**
+   * Copy a file from `fromPath` to `toPath`.
+   */
   readonly copyFile: (
     fromPath: string,
     toPath: string
   ) => Effect.Effect<never, PlatformError, void>
+  /**
+   * Change the permissions of a file.
+   */
   readonly chmod: (
     path: string,
     mode: number
   ) => Effect.Effect<never, PlatformError, void>
+  /**
+   * Change the owner and group of a file.
+   */
   readonly chown: (
     path: string,
     uid: number,
     gid: number
   ) => Effect.Effect<never, PlatformError, void>
+  /**
+   * Create a hard link from `fromPath` to `toPath`.
+   */
   readonly link: (
     fromPath: string,
     toPath: string
   ) => Effect.Effect<never, PlatformError, void>
+  /**
+   * Create a directory at `path`. You can optionally specify the mode and
+   * whether to recursively create nested directories.
+   */
   readonly makeDirectory: (
     path: string,
     options?: MakeDirectoryOptions
   ) => Effect.Effect<never, PlatformError, void>
+  /**
+   * Create a temporary directory.
+   *
+   * By default the directory will be created inside the system's default
+   * temporary directory, but you can specify a different location by setting
+   * the `directory` option.
+   *
+   * You can also specify a prefix for the directory name by setting the
+   * `prefix` option.
+   */
   readonly makeTempDirectory: (
     options?: MakeTempDirectoryOptions
   ) => Effect.Effect<never, PlatformError, string>
+  /**
+   * Create a temporary directory inside a scope.
+   *
+   * Functionally equivalent to `makeTempDirectory`, but the directory will be
+   * automatically deleted when the scope is closed.
+   */
   readonly makeTempDirectoryScoped: (
     options?: MakeTempDirectoryOptions
   ) => Effect.Effect<Scope, PlatformError, string>
+  /**
+   * Create a temporary file.
+   * The directory creation is functionally equivalent to `makeTempDirectory`.
+   * The file name will be a randomly generated string.
+   */
   readonly makeTempFile: (
     options?: MakeTempFileOptions
-  ) => Effect.Effect<Scope, PlatformError, File>
+  ) => Effect.Effect<never, PlatformError, string>
+  /**
+   * Create a temporary file inside a scope.
+   *
+   * Functionally equivalent to `makeTempFile`, but the file will be
+   * automatically deleted when the scope is closed.
+   */
+  readonly makeTempFileScoped: (
+    options?: MakeTempFileOptions
+  ) => Effect.Effect<Scope, PlatformError, string>
+  /**
+   * Open a file at `path` with the specified `options`.
+   *
+   * The file handle will be automatically closed when the scope is closed.
+   */
   readonly open: (
     path: string,
     options?: OpenFileOptions
   ) => Effect.Effect<Scope, PlatformError, File>
+  /**
+   * List the contents of a directory.
+   *
+   * You can recursively list the contents of nested directories by setting the
+   * `recursive` option.
+   */
   readonly readDirectory: (
     path: string,
     options?: ReadDirectoryOptions
   ) => Effect.Effect<never, PlatformError, ReadonlyArray<string>>
+  /**
+   * Read the contents of a file.
+   */
   readonly readFile: (
     path: string
   ) => Effect.Effect<never, PlatformError, Uint8Array>
+  /**
+   * Read the destination of a symbolic link.
+   */
   readonly readLink: (
     path: string
   ) => Effect.Effect<never, PlatformError, string>
+  /**
+   * Resolve a path to its canonicalized absolute pathname.
+   */
   readonly realPath: (
     path: string
   ) => Effect.Effect<never, PlatformError, string>
+  /**
+   * Remove a file or directory.
+   *
+   * By setting the `recursive` option to `true`, you can recursively remove
+   * nested directories.
+   */
   readonly remove: (
     path: string,
     options?: RemoveOptions
   ) => Effect.Effect<never, PlatformError, void>
+  /**
+   * Rename a file or directory.
+   */
   readonly rename: (
     oldPath: string,
     newPath: string
   ) => Effect.Effect<never, PlatformError, void>
+  /**
+   * Create a writable `Sink` for the specified `path`.
+   */
   readonly sink: (
     path: string,
     options?: SinkOptions
   ) => Sink<never, PlatformError, Uint8Array, never, void>
+  /**
+   * Get information about a file at `path`.
+   */
   readonly stat: (
     path: string
   ) => Effect.Effect<never, PlatformError, File.Info>
+  /**
+   * Create a readable `Stream` for the specified `path`.
+   *
+   * Changing the `bufferSize` option will change the internal buffer size of
+   * the stream. It defaults to `4`.
+   *
+   * The `chunkSize` option will change the size of the chunks emitted by the
+   * stream. It defaults to 64kb.
+   *
+   * Changing `offset` and `bytesToRead` will change the offset and the number
+   * of bytes to read from the file.
+   */
   readonly stream: (
     path: string,
     options?: StreamOptions
   ) => Stream<never, PlatformError, Uint8Array>
+  /**
+   * Create a symbolic link from `fromPath` to `toPath`.
+   */
   readonly symlink: (
     fromPath: string,
     toPath: string
   ) => Effect.Effect<never, PlatformError, void>
+  /**
+   * Truncate a file to a specified length. If the `length` is not specified,
+   * the file will be truncated to length `0`.
+   */
   readonly truncate: (
     path: string,
     length?: Size
   ) => Effect.Effect<never, PlatformError, void>
+  /**
+   * Change the file system timestamps of the file at `path`.
+   */
   readonly utimes: (
     path: string,
     atime: Date | number,
     mtime: Date | number
   ) => Effect.Effect<never, PlatformError, void>
+  /**
+   * Write data to a file at `path`.
+   */
   readonly writeFile: (
     path: string,
     data: Uint8Array,
