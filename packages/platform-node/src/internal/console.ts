bc2df955673fbd4cf2fffaba11db50087b4fad50
Tim <hello@timsmart.co>
2023-06-07 21:40:00 +1200

    chore: console

--- packages/platform-node/src/internal/console.ts
+++ packages/platform-node/src/internal/console.ts
@@ -4,14 +4,92 @@ import * as Console from "@effect/platform/Console"
 
 /** @internal */
 const consoleImpl = Console.Console.of({
+  assert(condition, ...args) {
+    return Effect.sync(() => {
+      console.assert(condition, ...args)
+    })
+  },
+  clear() {
+    return Effect.sync(() => {
+      console.clear()
+    })
+  },
+  count(label) {
+    return Effect.sync(() => {
+      console.count(label)
+    })
+  },
+  countReset(label) {
+    return Effect.sync(() => {
+      console.countReset(label)
+    })
+  },
+  debug(...args) {
+    return Effect.sync(() => {
+      console.debug(...args)
+    })
+  },
+  dir(...args) {
+    return Effect.sync(() => {
+      console.dir(...args)
+    })
+  },
+  dirxml(...args) {
+    return Effect.sync(() => {
+      console.dirxml(...args)
+    })
+  },
+  error(...args) {
+    return Effect.sync(() => {
+      console.error(...args)
+    })
+  },
+  group(options) {
+    return (self) =>
+      Effect.acquireUseRelease(
+        options?.collapsed ?
+          Effect.sync(() => console.groupCollapsed(options?.label)) :
+          Effect.sync(() => console.group(options?.label)),
+        () => self,
+        () => Effect.sync(() => console.groupEnd())
+      )
+  },
+  info(...args) {
+    return Effect.sync(() => {
+      console.info(...args)
+    })
+  },
   log(...args) {
     return Effect.sync(() => {
       console.log(...args)
     })
   },
-  logError(...args) {
+  table(tabularData, properties) {
     return Effect.sync(() => {
-      console.error(...args)
+      console.table(tabularData, properties)
+    })
+  },
+  time(label) {
+    return (self) =>
+      Effect.acquireUseRelease(
+        Effect.sync(() => console.time(label)),
+        () => self,
+        () => Effect.sync(() => console.timeEnd(label))
+      )
+  },
+  timeLog(label, ...args) {
+    return Effect.sync(() => {
+      console.timeLog(label, ...args)
+    })
+  },
+  trace(...args) {
+    return Effect.sync(() => {
+      console.trace(...args)
+    })
+  },
+  warn(...args) {
+    return Effect.sync(() => {
+      console.warn(...args)
     })
   }
 })
--- packages/platform/src/Console.ts
+++ packages/platform/src/Console.ts
@@ -2,14 +2,32 @@
  * @since 1.0.0
  */
 import * as Context from "@effect/data/Context"
+import type { Effect } from "@effect/io/Effect"
 
 /**
  * @since 1.0.0
  * @category model
  */
 export interface Console {
-  log(...args: Array<any>): void
-  logError(...args: Array<any>): void
+  assert(condition: boolean, ...args: Array<any>): Effect<never, never, void>
+  clear(): Effect<never, never, void>
+  count(label?: string): Effect<never, never, void>
+  countReset(label?: string): Effect<never, never, void>
+  debug(...args: Array<any>): Effect<never, never, void>
+  dir(...args: Array<any>): Effect<never, never, void>
+  dirxml(...args: Array<any>): Effect<never, never, void>
+  error(...args: Array<any>): Effect<never, never, void>
+  group(options?: {
+    readonly label?: string
+    readonly collapsed?: boolean
+  }): <R, E, A>(self: Effect<R, E, A>) => Effect<R, E, A>
+  info(...args: Array<any>): Effect<never, never, void>
+  log(...args: Array<any>): Effect<never, never, void>
+  table(tabularData: any, properties?: ReadonlyArray<string>): Effect<never, never, void>
+  time(label?: string): <R, E, A>(self: Effect<R, E, A>) => Effect<R, E, A>
+  timeLog(label?: string, ...args: Array<any>): Effect<never, never, void>
+  trace(...args: Array<any>): Effect<never, never, void>
+  warn(...args: Array<any>): Effect<never, never, void>
 }
 
 /**
