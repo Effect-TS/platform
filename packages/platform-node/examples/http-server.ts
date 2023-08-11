3a5221693ca252137a0360f8f372f24f87c91a4f
Tim <hello@timsmart.co>
2023-08-11 17:10:38 +1200

    update example

--- packages/platform-node/examples/http-server.ts
+++ packages/platform-node/examples/http-server.ts
@@ -1,5 +1,6 @@
 import * as Effect from "@effect/io/Effect"
 import * as Http from "@effect/platform-node/HttpServer"
+import { runMain } from "@effect/platform-node/Runtime"
 import { createServer } from "node:http"
 
 const ServerLive = Http.server.layer(() => createServer(), { port: 3000 })
@@ -8,5 +9,6 @@ const response = Http.response.text("hello world")
 Http.app.makeDefault((_req) => Effect.as(Effect.log("request"), response)).pipe(
   Http.server.serveJoin(Http.middleware.loggerTracer),
   Effect.provideLayer(ServerLive),
-  Effect.runPromise
+  Effect.tapErrorCause(Effect.logError),
+  runMain
 )
