293ea634d80d62778610d0e484d5dec8450bb351
Jesse Kelly <kellyjesse881@gmail.com>
2023-08-28 19:42:05 -0500

    chore: added session storage test

--- packages/platform-browser/test/KeyValueStore.test.ts
+++ packages/platform-browser/test/KeyValueStore.test.ts
@@ -3,3 +3,4 @@ import * as Kv from "@effect/platform-browser/KeyValueStore"
 import { testLayer } from "@effect/platform/test/KeyValueStore.test"
 
 describe("KeyValueStore / layerLocalStorage", () => testLayer(Kv.layerLocalStorage))
+describe("KeyValueStore / layerSessionStorage", () => testLayer(Kv.layerSessionStorage))
