be1b6f036246713a55462ec76e8d999eae654cd7
Maxwell Brown <maxwellbrown1990@gmail.com>
2023-09-20 00:07:51 -0400

    Make `platform-node` an explicit dependency of `platform-bun` (#153)

--- flake.nix
+++ flake.nix
@@ -17,14 +17,19 @@
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
+      corepackEnable = pkgs.runCommand "corepack-enable" {} ''
+        mkdir -p $out/bin
+        ${pkgs.nodejs-18_x}/bin/corepack enable --install-directory $out/bin
+      '';
     in {
       formatter = pkgs.alejandra;
 
       devShells = {
         default = pkgs.mkShell {
           buildInputs = with pkgs; [
+            bun
             nodejs-18_x
-            nodePackages.pnpm
+            corepackEnable
           ];
         };
       };
