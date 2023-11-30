d7e5114741980cf173904cfac8e97742cb7091e1
Leo Conforti <leo@leoconforti.us>
2023-11-30 07:52:57 +0000

    lint

--- packages/platform-node/src/Http/NodeClient.ts
+++ packages/platform-node/src/Http/NodeClient.ts
@@ -62,8 +62,7 @@ export const makeAgentLayer: (options?: Https.AgentOptions) => Layer.Layer<never
  * @since 1.0.0
  * @category agent
  */
-export const fromAgent: (agent: HttpAgent) => Client.Client.Default =
-  internal.fromAgent
+export const fromAgent: (agent: HttpAgent) => Client.Client.Default = internal.fromAgent
 
 /**
  * @since 1.0.0
